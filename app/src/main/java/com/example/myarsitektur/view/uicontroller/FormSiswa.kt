3503fbeb81dfaf4a623187fa5187d76222585926
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2025-11-20 17:49:27 +0700

    Membuat tombol submit

--- app/src/main/java/com/example/myarsitektur/view/uicontroller/FormSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/uicontroller/FormSiswa.kt
@@ -116,5 +116,19 @@ fun FormSiswa(
                 }
             )
             Spacer(Modifier.height(20.dp))
-            
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+
+                enabled = txtNama.isNotEmpty() && txtAlamat.isNotEmpty() && txtGender.isNotEmpty(),
+                onClick = {
+
+                    val listData = mutableListOf(txtNama, txtGender, txtAlamat)
+                    onSubmitButtonClicked(listData)
+                }
+            ) {
+                Text(text = stringResource(R.string.submit))
+            }
+        }
+    }
+}
 
