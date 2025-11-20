7b46306011b94b7c962c4934e46c3b6701877e8c
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2025-11-20 17:45:48 +0700

    Membuat TopAppBar di FormSiswa

--- app/src/main/java/com/example/myarsitektur/view/uicontroller/FormSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/uicontroller/FormSiswa.kt
@@ -43,3 +43,14 @@ fun FormSiswa(
     var txtAlamat by rememberSaveable { mutableStateOf("") }
     var txtGender by remember { mutableStateOf("") }
 
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.purple_500)
+                )
+            )
+        }
+    )
